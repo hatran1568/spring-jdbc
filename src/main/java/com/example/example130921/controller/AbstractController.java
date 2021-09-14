4c4e46c1ad2057fc5734e6d0c844894f7287e355
Tran Thi Nguyet Ha <hattn@vietis.com.vn>
2021-09-14 11:00:03 +0700

    Update

--- src/main/java/com/example/example130921/controller/AbstractController.java
+++ src/main/java/com/example/example130921/controller/AbstractController.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 
 public abstract class AbstractController<s> {
 
+    @Autowired
     protected s service;
 
     protected <T> ResponseEntity<T> response(Optional<T> response) {
